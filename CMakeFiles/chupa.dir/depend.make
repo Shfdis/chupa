527ab20375f06b2b7abe0c95ece9ee4bfac97f4f
NoDeRE <s24v_kononov@179.ru>
2022-12-16 11:57:36 +0300

    Cmake config

---
+++ CMakeFiles/chupa.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for chupa.
+# This may be replaced when dependencies are built.
---
+++ CMakeFiles/chupa.dir/gameclasses/player.cpp.o.d
@@ -0,0 +1,3 @@
+CMakeFiles/chupa.dir/gameclasses/player.cpp.o: \
+ /home/oleg/chupa/chupa/gameclasses/player.cpp /usr/include/stdc-predef.h \
+ /home/oleg/chupa/chupa/gameclasses/player.h
