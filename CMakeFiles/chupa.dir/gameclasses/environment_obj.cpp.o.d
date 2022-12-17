43f01ac5c4f7d9a9aca1465bd8815ec7a8c56374
shfs <huaweitit0101@gmail.com>
2022-12-17 14:07:08 +0300

    Revert "ignore this shit"
    This reverts commit 51ebd4eddf8a5924779aa2231a73bf0a8ff5b287.

---
+++ CMakeFiles/chupa.dir/gameclasses/environment_obj.cpp.o.d
@@ -0,0 +1,4 @@
+CMakeFiles/chupa.dir/gameclasses/environment_obj.cpp.o: \
+ /home/oleg/chupa/chupa/gameclasses/environment_obj.cpp \
+ /usr/include/stdc-predef.h \
+ /home/oleg/chupa/chupa/gameclasses/environment_obj.h
--- cmake_install.cmake
+++ cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/artem/chupa
+# Install script for directory: /home/oleg/chupa/chupa
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -42,22 +42,6 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
-if(CMAKE_INSTALL_COMPONENT STREQUAL "Unspecified" OR NOT CMAKE_INSTALL_COMPONENT)
-  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/chupa" AND
-     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/chupa")
-    file(RPATH_CHECK
-         FILE "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/chupa"
-         RPATH "")
-  endif()
-  file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/bin" TYPE EXECUTABLE FILES "/home/artem/chupa/chupa")
-  if(EXISTS "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/chupa" AND
-     NOT IS_SYMLINK "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/chupa")
-    if(CMAKE_INSTALL_DO_STRIP)
-      execute_process(COMMAND "/usr/bin/strip" "$ENV{DESTDIR}${CMAKE_INSTALL_PREFIX}/bin/chupa")
-    endif()
-  endif()
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -66,5 +50,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/artem/chupa/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/oleg/chupa/chupa/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
