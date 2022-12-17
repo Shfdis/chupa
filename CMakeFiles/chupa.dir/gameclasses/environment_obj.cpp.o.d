51ebd4eddf8a5924779aa2231a73bf0a8ff5b287
shfs <huaweitit0101@gmail.com>
2022-12-17 10:23:53 +0300

    ignore this shit

--- CMakeFiles/chupa.dir/gameclasses/environment_obj.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/chupa.dir/gameclasses/environment_obj.cpp.o: \
- /home/oleg/chupa/chupa/gameclasses/environment_obj.cpp \
- /usr/include/stdc-predef.h \
- /home/oleg/chupa/chupa/gameclasses/environment_obj.h
--- CMakeFiles/chupa.dir/gameclasses/player.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/chupa.dir/gameclasses/player.cpp.o: \
- /home/oleg/chupa/chupa/gameclasses/player.cpp /usr/include/stdc-predef.h \
- /home/oleg/chupa/chupa/gameclasses/player.h
