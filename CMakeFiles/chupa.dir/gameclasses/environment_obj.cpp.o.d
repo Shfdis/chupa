63473c506cdc2763773febbf4099222351242d42
NoDeRE <s24v_kononov@179.ru>
2022-12-24 13:42:11 +0300

    brainisfucked

--- CMakeFiles/chupa.dir/gameclasses/environment_obj.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/chupa.dir/gameclasses/environment_obj.cpp.o: \
- /home/artem/chupa/gameclasses/environment_obj.cpp \
- /usr/include/stdc-predef.h \
- /home/artem/chupa/gameclasses/environment_obj.h
